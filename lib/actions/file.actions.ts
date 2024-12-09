da60a030af80888b90714b9aa4ed14ac6c39bb3d
Arpita Sharma <arpita.arusharma@gmail.com>
2024-12-09 19:38:35 +0530

    adding rename functionality to images

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -8,6 +8,7 @@ import { ID, Models, Query } from "node-appwrite";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
 import { getCurrentUser } from "./user.actions";
+import path from "path";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -85,3 +86,27 @@ export const getFiles = async () => {
     handleError(error, "Failed to get files");
   }
 };
+
+export const renameFile = async ({
+  fileId,
+  name,
+  extension,
+  path,
+}: RenameFileProps) => {
+  const { databases } = await createAdminClient();
+  try {
+    const newName = `${name}.${extension}`;
+    const updatedFile = await databases.updateDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      fileId,
+      {
+        name: newName,
+      },
+    );
+    revalidatePath(path);
+    return parseStringify(updatedFile);
+  } catch (error) {
+    handleError(error, "Failed to rename file");
+  }
+}; 
